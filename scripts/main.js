08c8fd543a7ef2ee5fbb01fe17dd401c46b03871
Christian Seidel <github@parameter5.de>
2022-07-26 23:11:06 +0200

    initial

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+let alarm = new Audio('sound/alarm.mp3');
+
+let timerStarted = false;
+
+function startTimer() {
+    changeBackground();
+    if (!timerStarted) {
+        let startTime = new Date().getTime();
+        let fiveMinutes = 1000 * 60 * 5;
+        let endTime = startTime + fiveMinutes;
+
+        setInterval(function () {
+            let timeLeft = endTime - new Date().getTime();
+
+            if (timeLeft > 0) {
+                let minutes = timeLeft / (1000 * 60);
+                minutes = Math.floor(minutes);
+                let seconds = (timeLeft / 1000) % 60;
+                seconds = Math.round(seconds);
+                seconds = ('0' + seconds).slice(-2);
+                let text = '0' + minutes + ' : ' + seconds;
+                timer.innerHTML = text;
+            } else {
+                alarm.play();
+                timer.innerHTML = '00 : 00'
+            }
+        }, 1000);
+        timerStarted = true;
+    }
+}
+
+
+let backgroundToggle = true;
+
+function changeBackground() {
+    document.body.className = backgroundToggle
+        ? "background-blue"
+        : "background-black";
+    backgroundToggle = !backgroundToggle;
+}
+
