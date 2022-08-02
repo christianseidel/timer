95c379d7beae5de7878d46b2f67ea9f3138c5022
Christian Seidel <github@parameter5.de>
2022-08-02 23:22:01 +0200

    new version

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,29 @@
 let alarm = new Audio('sound/alarm.mp3');
-
+let input = 2;
+let numberOfMinutes = 1000 * 60 * input;
+let timer = document.getElementById('timer');
 let timerStarted = false;
 
+let displayStartBtn = true;
+document.getElementById('stopper').style.display = 'none';
+
+document.getElementById('starter').addEventListener('click', startTimer);
+
 function startTimer() {
-    changeBackground();
-    if (!timerStarted) {
+        if (!timerStarted) {
+        timerStarted = false;
+        console.log('Timer started ' + timerStarted);
+        toggleStartBtn();
+        changeBackground();
         let startTime = new Date().getTime();
-        let fiveMinutes = 1000 * 60 * 5;
-        let endTime = startTime + fiveMinutes;
+        numberOfMinutes = 1000 * 60 * input;
+        console.log(input);
+        let endTime = startTime + numberOfMinutes;
 
-        setInterval(function () {
+        const idTimer = setInterval(runTimer, 1000);
+
+        function runTimer() {
             let timeLeft = endTime - new Date().getTime();
-
             if (timeLeft > 0) {
                 let minutes = timeLeft / (1000 * 60);
                 minutes = Math.floor(minutes);
@@ -21,21 +33,59 @@ function startTimer() {
                 let text = '0' + minutes + ' : ' + seconds;
                 timer.innerHTML = text;
             } else {
-                alarm.play();
+                clearInterval(idTimer);
+                timerStarted = false;
                 timer.innerHTML = '00 : 00'
+                alarm.play();
+                console.log('end');
             }
-        }, 1000);
-        timerStarted = true;
+
+            document.getElementById('stopper').addEventListener('click', pauseTimer);
+
+            function pauseTimer() {
+                clearInterval(idTimer);
+                timerStarted = false;
+                console.log('Timer stopped! (' + timerStarted + ')' )
+                toggleStartBtn();
+            }
+        }
     }
 }
 
 
-let backgroundToggle = true;
-
-function changeBackground() {
-    document.body.className = backgroundToggle
-        ? "background-blue"
-        : "background-black";
-    backgroundToggle = !backgroundToggle;
+function toggleStartBtn() {
+    console.log('toggle button: ' + displayStartBtn);
+    if (displayStartBtn) {
+        document.getElementById('starter').style.display = 'none';
+        document.getElementById('stopper').style.display = 'block';
+    } else {
+        document.getElementById('starter').style.display = 'block';
+        document.getElementById('stopper').style.display = 'none';
+        }
+    displayStartBtn = !displayStartBtn;
 }
+
+let toggleBackground = true;
+function changeBackground() {
+    document.body.className = toggleBackground
+        ? 'background-blue'
+        : 'background-black';
+    toggleBackground = !toggleBackground;
+}
+
+function validateInput() {
+    input = document.getElementById('time-round_1').value;
+    if (input < 1) {
+        alert('Please enter the number of minutes required.\n' + input + ' minutes is not a valid input.')
+    }
+    document.getElementById('timer').innerText = ((input < 10) ? '0' : '') + input + ' : 00';
+    document.getElementById('label-round_1').innerHTML = (input == 1 ? 'Minute \u00A0' : 'Minuten');
+}
+
+function resetInput() {
+    input = 2;
+    document.getElementById('timer').innerText = '02 : 00';
+    document.getElementById('label-round_1').innerHTML = 'Minuten';
+}
+
 
---
+++ scripts/timer.js
@@ -0,0 +1,89 @@
+let alarm = new Audio('sound/alarm.mp3');
+document.getElementById('btn-start').addEventListener('click', startTimer);
+
+let timer = document.getElementById('timer');
+let m = 0.05;
+let mSaved = m;
+let minutes = 0;
+let seconds = 0;
+let text ='';
+showTime(1000 * 60 * m);
+let timerStarted = false;
+
+function startTimer() {
+
+    if (!timerStarted) {
+        let startTime = new Date().getTime();
+        let endTime = startTime + (1000 * 60 * m);
+
+        timerStarted = true;
+        console.log('timer started');
+
+        const myTimer = setInterval(runTimer, 1000);
+
+        function runTimer() {
+            let timeLeft = endTime - new Date().getTime();
+
+            if (timeLeft > 0) {
+                showTime(timeLeft);
+            } else {
+                clearInterval(myTimer);
+                timerStarted = false;
+                alarm.play();
+                timer.innerHTML = '00 : 00';
+                setTimeout(function () {
+                    m = mSaved;
+                    showTime(1000 * 60 * m);
+                    console.log('lapse terminated')
+                }, 11000);
+            }
+        }
+
+        document.getElementById('btn-stop').addEventListener('click', stopTimer);
+
+        function stopTimer() {
+            if (timerStarted) {
+                clearInterval(myTimer);
+                timerStarted = false;
+                console.log('timer stopped');
+                let timeRemaining = timer.innerHTML;
+                console.log(timeRemaining);
+                // yet to calculate m...
+
+            } else {
+                showMessage('noStop', 'Timer isn\'t running');
+            }
+        }
+    } else {
+        showMessage('noRun', 'Timer already started');
+    }
+}
+
+function showTime(timeLeft) {
+    minutes = Math.floor(timeLeft / (1000 *60));
+    seconds = Math.round(timeLeft / 1000) % 60;
+    seconds = ('0' + seconds).slice(-2);
+    text = '0' + minutes + ' : ' + seconds;
+    timer.innerHTML = text;
+}
+
+document.getElementById('btn-reset').addEventListener('click', reset);
+
+function reset() {
+    if (timerStarted) {
+        showMessage('noReset', 'Timer is still running')
+    } else {
+        m = mSaved;
+        showTime(1000 * 60 * m);
+    }
+}
+
+function showMessage(idClip, message) {
+    document.getElementById('msg_' + idClip).style.display = 'block';
+    document.getElementById('msg_' + idClip).innerHTML = message;
+    setTimeout(function () {
+        document.getElementById('msg_' + idClip).style.display = 'none';
+    }, 2000);
+
+}
+
